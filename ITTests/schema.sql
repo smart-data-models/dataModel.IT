755c5219068392f7ea743ac21e9cd1f665e962b5
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:12:52 +0200

    test of SQL export

--- ITTests/schema.sql
+++ ITTests/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model ITTests of the subject dataModel.IT for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE status_type AS ENUM ('OK','NOK','POK');CREATE TYPE ITTests_type AS ENUM ('ITTests');
-CREATE TABLE ITTests (address JSON, alternateName TEXT, areaServed TEXT, component TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, elapsedTime NUMERIC, name TEXT, owner JSON, source TEXT, status status_type, tests JSON, timestamp NUMERIC, type ITTests_type);
+CREATE TABLE ITTests (address JSON, alternateName TEXT, areaServed TEXT, component TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, elapsedTime NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, status status_type, tests JSON, timestamp NUMERIC, type ITTests_type);
