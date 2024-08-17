9df263ab6ae1cd569cd1f2270edfd3474e387d3d
Alberto Abella <alberto.abella@fiware.org>
2024-08-18 01:11:43 +0200

    Draft version of pydantic file v2

---
+++ ITTests/code/code_for_using_pydantic.py
@@ -0,0 +1,219 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import (
+    AnyUrl,
+    AwareDatetime,
+    BaseModel,
+    Field,
+    RootModel,
+    confloat,
+    constr,
+)
+
+
+class Address(BaseModel):
+    addressCountry: Optional[str] = Field(
+        None, description='The country. For example, Spain'
+    )
+    addressLocality: Optional[str] = Field(
+        None,
+        description='The locality in which the street address is, and which is in the region',
+    )
+    addressRegion: Optional[str] = Field(
+        None,
+        description='The region in which the locality is, and which is in the country',
+    )
+    district: Optional[str] = Field(
+        None,
+        description='A district is a type of administrative division that, in some countries, is managed by the local government',
+    )
+    postOfficeBoxNumber: Optional[str] = Field(
+        None,
+        description='The post office box number for PO box addresses. For example, 03578',
+    )
+    postalCode: Optional[str] = Field(
+        None, description='The postal code. For example, 24004'
+    )
+    streetAddress: Optional[str] = Field(None, description='The street address')
+    streetNr: Optional[str] = Field(
+        None, description='Number identifying a specific property on a public street'
+    )
+
+
+class Type(Enum):
+    Point = 'Point'
+
+
+class Location(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[float] = Field(..., min_length=2)
+    type: Type
+
+
+class Coordinate(RootModel[List[float]]):
+    root: List[float]
+
+
+class Type1(Enum):
+    LineString = 'LineString'
+
+
+class Location1(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[Coordinate] = Field(..., min_length=2)
+    type: Type1
+
+
+class Type2(Enum):
+    Polygon = 'Polygon'
+
+
+class Location2(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type2
+
+
+class Type3(Enum):
+    MultiPoint = 'MultiPoint'
+
+
+class Location3(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[float]]
+    type: Type3
+
+
+class Type4(Enum):
+    MultiLineString = 'MultiLineString'
+
+
+class Location4(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[Coordinate]]
+    type: Type4
+
+
+class Type5(Enum):
+    MultiPolygon = 'MultiPolygon'
+
+
+class Location5(BaseModel):
+    bbox: Optional[List[float]] = Field(None, min_length=4)
+    coordinates: List[List[List[Coordinate]]]
+    type: Type5
+
+
+class Status(Enum):
+    OK = 'OK'
+    NOK = 'NOK'
+    POK = 'POK'
+
+
+class Result(Enum):
+    OK = 'OK'
+    NOK = 'NOK'
+    NA = 'NA'
+    DISMISS = 'DISMISS'
+
+
+class Test(BaseModel):
+    name: str = Field(
+        ..., description='The name or identification to the test executed'
+    )
+    result: Result = Field(
+        ...,
+        description='The result of the execution of the test. it could be:OK, the test passed; NOK, the test failed; N/A, the test was not executed due to it was not applicable for the corresponding software or component; DISMISS, the test could not be executed due to a previous error in other test',
+    )
+
+
+class Type6(Enum):
+    ITTests = 'ITTests'
+
+
+class ITTests(BaseModel):
+    address: Optional[Address] = Field(None, description='The mailing address')
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    areaServed: Optional[str] = Field(
+        None,
+        description='The geographic area where a service or offered item is provided',
+    )
+    component: Optional[AnyUrl] = Field(
+        None,
+        description='Identifier format of any NGSI entity corresponding to the Software or component to be tested',
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    elapsedTime: Optional[confloat(ge=0.0, multiple_of=1.0)] = Field(
+        None,
+        description='Elapsed time since the beginning of the execution of the tests. It measures in milliseconds the duration of the execution of the tests',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    location: Optional[
+        Union[Location, Location1, Location2, Location3, Location4, Location5]
+    ] = Field(
+        None,
+        description='Geojson reference to the item. It can be Point, LineString, Polygon, MultiPoint, MultiLineString or MultiPolygon',
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    status: Optional[Status] = Field(
+        None,
+        description='Overall status of the execution of the tests: OK, all the tests passed; NOK, there is at least some tests that failed; POK, it is possible that some tests are marked as optionals, therefore it they fails the overall status is not NOK but POK',
+    )
+    tests: Optional[List[Test]] = Field(
+        None, description='Status of the execution of each of the tests'
+    )
+    timestamp: Optional[confloat(ge=0.0, multiple_of=1.0)] = Field(
+        None,
+        description='EPOCH timestamp measures in milliseconds corresponding to the starting point of the execution of the tests',
+    )
+    type: Optional[Type6] = Field(
+        None, description='NGSI Entity type. It has to be ITTests'
+    )
